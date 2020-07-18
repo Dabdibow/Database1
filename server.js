177c42c6781cc0b51987f6618b5e8aeacc1b00ae
Dabdibow <64368085+Dabdibow@users.noreply.github.com>
2020-07-18 13:49:27 +0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-require('./app/routes')(app);
-app.use(express.static('public'));
-app.listen(80);
-console.log("Server started at 80");
